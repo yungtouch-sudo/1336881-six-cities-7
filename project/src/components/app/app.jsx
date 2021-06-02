99a7a466c19eda9f8cef86cd2a6b6aca6dd1ce69
yungtouch-sudo <exinterp@aol.com>
2021-06-02 13:48:34 -0700

    render full page/cards/cardsTitles

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import PageScreen from '../pagescreen/pagescreen';
+
+const cardsDetail = [
+  { 'id': 1, 'title': 'Beautiful &amp; luxurious apartment at great location' },
+  { 'id': 2, 'title': 'Wood and stone place' },
+  { 'id': 3, 'title': 'Canal View Prinsengracht' },
+  { 'id': 4, 'title': 'Nice, cozy, warm big bed apartment' },
+  { 'id': 5, 'title': 'Wood and stone place' },
+];
+
 
 function App() {
-  return <p>Hello, world!</p>;
+
+  return (
+    <PageScreen cardsDetail={cardsDetail} />
+  );
 }
 
 export default App;
