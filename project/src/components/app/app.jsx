dc39e860af668b6040494dc1f5691146d15a96d3
yungtouch-sudo <exinterp@aol.com>
2021-06-05 11:04:55 -0700

    add routes

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
 import PageScreen from '../pagescreen/pagescreen';
+import Login from '../login/login';
+import Favorites from '../favorites/favorites';
+import Offer from '../offer/offer';
+import Err404 from '../err404/err404';
 
 const cardsDetail = [
   { 'id': 1, 'title': 'Beautiful &amp; luxurious apartment at great location' },
@@ -9,11 +15,36 @@ const cardsDetail = [
   { 'id': 5, 'title': 'Wood and stone place' },
 ];
 
+const AppRoute = {
+  ROOT: '/',
+  LOGIN: '/login',
+  FAVORITES: '/favorites',
+  ROOM: '/offer/:id',
+};
+
 
 function App() {
 
   return (
-    <PageScreen cardsDetail={cardsDetail} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.ROOT}>
+          <PageScreen cardsDetail={cardsDetail} />
+        </Route>
+        <Route exact path={AppRoute.LOGIN}>
+          <Login />
+        </Route>
+        <Route exact path={AppRoute.FAVORITES}>
+          <Favorites />
+        </Route>
+        <Route exact path={AppRoute.ROOM}>
+          <Offer />
+        </Route>
+        <Route path={'/'}>
+          <Err404 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
